4779714434804dbdf19aa563bcef809702a3942a
nourinfathima <nourinnourin36@gmail.com>
2022-11-02 08:40:21 +0530

    first commit

---
+++ employe.py
@@ -0,0 +1,43 @@
+while True:
+
+    print("select an option from menu")
+
+    print("1 add employe")
+
+    print("2 view all employe")
+
+    print("3 search a employe")
+
+    print("4 update the employe")
+
+    print("5 delete a employe")
+
+    print("6 exit")
+
+
+
+    choice = int(input("Enter an option: "))
+
+    if(choice==1):
+
+        print("employe enter selected")
+
+    elif(choice==2):
+
+        print("view employe selected")
+
+    elif(choice==3):
+
+        print("search employe selected")
+
+    elif(choice==4):
+
+        print("update employe selected")
+
+    elif(choice==5):
+
+        print("delete employe selected")
+
+    elif(choice==6):
+
+        break
